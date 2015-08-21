6466b4ace605e42f48bf0dd16386dd3f81899186
cpn <cpn@cpn>
2015-08-21 20:53:32 +0400

    Цепочка ответственности на основе сопрограмм

---
+++ behavioral_patterns/chain_of_responsibility.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import functools
+
+__author__ = 'cpn'
+
+
+"""
+Цепочка ответственности на основе сопрограмм
+"""
+
+
+def coroutine(function):
+    @functools.wraps(function)
+    def wrapper(*args, **kwargs):
+        generator = function(*args, **kwargs)
+        next(generator)
+        return generator
+    return wrapper
+
+
+
+@coroutine
+def key_handler(successor=None):
+    while True:
+        event = (yield)
+        if event.kind == Event.KEYPRESS:
+            print ("event")
+        elif successor is not None:
+            successor.send(event)
+
+
